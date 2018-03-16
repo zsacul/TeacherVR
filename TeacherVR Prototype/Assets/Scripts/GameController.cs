74a635e8211e497dd702784a76adc9a7bc461c7c
Senello <matoz1995@gmail.com>
2018-03-17 00:50:01 +0100

    GameController Singleton and little changes

---
+++ TeacherVR Prototype/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    #region Singleton
+
+    public static GameController Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one intence of GameController found!");
+            return;
+        }
+        Instance = this;
+    }
+
+    #endregion
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+    
+}
