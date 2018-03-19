f495fdfb6f1eaab0d5fefc513e0c7c7246e60841
Andrzej Milczarek <milczarek96@gmail.com>
2018-03-19 23:01:38 +0100

    Chalk Throwing on Sphere

---
+++ TeacherVR Prototype/Assets/Scripts/Chalk_Throwing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chalk_Throwing : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("Chalk"))
+        {
+            Debug.Log("It's ALIVE and red");
+            transform.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+}
