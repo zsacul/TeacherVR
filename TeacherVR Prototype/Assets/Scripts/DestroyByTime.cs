c05a9938fe815e1c1032602576d066aebc84d8aa
Senello <matoz1995@gmail.com>
2018-03-19 22:27:09 +0100

    Eggcellent update3 and upgraded DestroyByTime for SnapDropZones

---
+++ TeacherVR Prototype/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float LifeTime;
+    public bool SnapDropZone = false;
+
+    private bool wasKinematic = false;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (!SnapDropZone) Destroy(gameObject, LifeTime);
+    }
+
+    void Update()
+    {
+        if (rb != null)
+            if (rb.isKinematic)
+            {
+                wasKinematic = true;
+            }
+            else if(wasKinematic)
+            {
+                Destroy(gameObject, LifeTime);
+            }
+    }
+}
