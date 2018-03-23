ca17e2b6247d3ee4d72b23422c860a368aeb8f7d
Senello <33639608+Senello@users.noreply.github.com>
2018-03-23 13:28:12 +0100

    Events V1

---
+++ TeacherVR Prototype/Assets/Scripts/Events/Scripts/Events.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Events/Basic Event")]
+public class Events : ScriptableObject
+{
+    new public string name = "New Event";
+    public Sprite icon = null;
+    public string description;
+    public Status status;
+    public int lvl;
+
+    public virtual void StartEvent()
+    {
+        Debug.Log("Starting " + name);
+    }
+
+    public virtual void AbortEvent()
+    {
+        Debug.Log("Aborting " + name);
+    }
+
+    public enum Status
+    {
+        Nothing,
+        Progress,
+        Complete
+    }
+
+    public void Pkt(int pkt)
+    {
+        Debug.Log("+Pkt " + name);
+    }
+}
---
+++ TeacherVR Prototype/Assets/Scripts/TouchTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("LController") || other.tag.Equals("RController"))
+        {
+            Debug.Log(other.tag);
+        }
+        Debug.Log(other.tag);
+    }
+}
