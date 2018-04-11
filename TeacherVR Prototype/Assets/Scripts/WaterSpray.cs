cf1a33b97aeb5c2e2f744ca1f08d888b1221a56c
Senello <matoz1995@gmail.com>
2018-04-12 01:28:38 +0200

    Water Spray

---
+++ TeacherVR Prototype/Assets/Scripts/WaterSpray.cs
@@ -0,0 +1,86 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class WaterSpray : SwitchControllerObject
+    {
+        public float bulletSpeed = 200f;
+        public float bulletLife = 5f;
+        public float fireDelay = 0.5f;
+
+        private float lastFire = 0f;
+
+        private GameObject bullet;
+        private GameObject trigger;
+
+        private PourWater pourWater;
+
+        private float minTriggerRotation = -10f;
+        private float maxTriggerRotation = 45f;
+        
+        public override void Grabbed(VRTK_InteractGrab currentGrabbingObject)
+        {
+            base.Grabbed(currentGrabbingObject);
+            
+            //Limit hands grabbing when picked up
+            if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject.controllerEvents.gameObject) ==
+                SDK_BaseController.ControllerHand.Left)
+            {
+                allowedTouchControllers = AllowedController.LeftOnly;
+                allowedUseControllers = AllowedController.LeftOnly;
+            }
+            else if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject.controllerEvents.gameObject) ==
+                     SDK_BaseController.ControllerHand.Right)
+            {
+                allowedTouchControllers = AllowedController.RightOnly;
+                allowedUseControllers = AllowedController.RightOnly;
+            }
+        }
+
+        public override void StartUsing(VRTK_InteractUse currentUsingObject)
+        {
+            base.StartUsing(currentUsingObject);
+                if (Time.time > lastFire + fireDelay && pourWater.Use())
+                {
+                    lastFire = Time.time;
+                    FireBullet();
+                    VRTK_ControllerHaptics.TriggerHapticPulse(
+                        VRTK_ControllerReference.GetControllerReference(ce.gameObject), 0.63f, 0.2f, 0.01f);
+                }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            bullet = transform.Find("Bullet").gameObject;
+            pourWater = GetComponentInChildren<PourWater>();
+            bullet.SetActive(false);
+
+            trigger = transform.Find("Head/Trigger").gameObject;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (ce)
+            {
+                var pressure = (maxTriggerRotation * ce.GetTriggerAxis()) - minTriggerRotation;
+                trigger.transform.localEulerAngles = new Vector3(0f, pressure, 0f);
+            }
+            else
+            {
+                trigger.transform.localEulerAngles = new Vector3(0f, minTriggerRotation, 0f);
+            }
+        }
+
+        private void FireBullet()
+        {
+            GameObject bulletClone =
+                Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
+            bulletClone.SetActive(true);
+            Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+            rb.AddForce(bullet.transform.forward * bulletSpeed);
+            Destroy(bulletClone, bulletLife);
+        }
+    }
+}
---
+++ TeacherVR Prototype/Assets/Shaders/AlanZucconi/PourWater.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PourWater : MonoBehaviour
+{
+    public Material material;
+
+    public float speed;
+    public float cost;
+    public float maxFuel;
+    public float offset;
+
+    public Transform Top;
+
+    private float fuel;
+    private float diff;
+    private void Start()
+    {
+        fuel = 0;
+    }
+
+    private void Update()
+    {
+        diff = (Top.transform.position.y - transform.position.y);
+        if(diff>0) material.SetFloat("_ConstructY", transform.position.y - offset + (fuel/maxFuel)*diff);
+        else material.SetFloat("_ConstructY", Top.transform.position.y - offset + (fuel / maxFuel) * (transform.position.y - Top.transform.position.y));
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        if (col.tag.Equals("WaterSource"))
+        {
+            if (fuel <= maxFuel)
+                fuel = Mathf.Lerp(fuel, fuel + cost,
+                    speed * Time.deltaTime);
+        }
+    }
+
+    public bool Use()
+    {
+        if (fuel > cost)
+        {
+            fuel -= cost;
+            return true;
+        }
+        return false;
+    }
+}
