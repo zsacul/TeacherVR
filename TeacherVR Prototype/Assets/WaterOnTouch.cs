2b57c28948a93565382c0a69b3766252213ad1b0
Senello <matoz1995@gmail.com>
2018-04-17 00:09:27 +0200

    water sound update

--- TeacherVR Prototype/Assets/WaterOnTouch.cs
+++ TeacherVR Prototype/Assets/WaterOnTouch.cs
@@ -6,25 +6,26 @@ using Valve.VR;
 public class WaterOnTouch : MonoBehaviour
 {
     public GameObject WaterSource;
+    public float WaterDurration = 5;
 
     private Animator anim;
     private float lastTime;
-    private float durration;
+    private float soundDurration;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         WaterSource.SetActive(anim.GetBool("On"));
         lastTime = Time.time;
-        durration = GameController.Instance.SoundManager.SfxWaterRunning.length-0.1f;
+        soundDurration = GameController.Instance.SoundManager.SfxWaterRunning.length-0.1f;
     }
 
     void Update()
     {
-        if (WaterSource.activeSelf && Time.time > lastTime + durration)
+        if (WaterSource.activeSelf && Time.time > lastTime + soundDurration)
         {
             lastTime = Time.time;
-            GameController.Instance.SoundManager.Play3DAt(SamplesList.WaterRunning, WaterSource.transform.position);
+            GameController.Instance.SoundManager.Play3DAt(SamplesList.WaterRunning, WaterSource.transform.position,0.05f);
         }
     }
 
@@ -32,8 +33,21 @@ public class WaterOnTouch : MonoBehaviour
     {
         if (col.tag.Equals("LController") || col.tag.Equals("RController"))
         {
-            anim.SetBool("On", !anim.GetBool("On"));
-            WaterSource.SetActive(anim.GetBool("On"));
+            StopAllCoroutines();
+            ChangeState();
+            StartCoroutine(TurnOff());
         }
+    }
+
+    private void ChangeState()
+    {
+        anim.SetBool("On", !anim.GetBool("On"));
+        WaterSource.SetActive(anim.GetBool("On"));
+    }
+
+    IEnumerator TurnOff()
+    {
+        yield return new WaitForSeconds(WaterDurration);
+        ChangeState();
     }
 }
