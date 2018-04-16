cb504fe5278705d20f12d7ccc61d36b46df2c888
Senello <matoz1995@gmail.com>
2018-04-16 23:10:21 +0200

    Taps

---
+++ TeacherVR Prototype/Assets/WaterOnTouch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class WaterOnTouch : MonoBehaviour
+{
+    public GameObject WaterSource;
+
+    private Animator anim;
+    private float lastTime;
+    private float durration;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        WaterSource.SetActive(anim.GetBool("On"));
+        lastTime = Time.time;
+        durration = GameController.Instance.SoundManager.SfxWaterRunning.length-0.1f;
+    }
+
+    void Update()
+    {
+        if (WaterSource.activeSelf && Time.time > lastTime + durration)
+        {
+            lastTime = Time.time;
+            GameController.Instance.SoundManager.Play3DAt(SamplesList.WaterRunning, WaterSource.transform.position);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag.Equals("LController") || col.tag.Equals("RController"))
+        {
+            anim.SetBool("On", !anim.GetBool("On"));
+            WaterSource.SetActive(anim.GetBool("On"));
+        }
+    }
+}
