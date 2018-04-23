1ad557a88db8096cc87bdef5b99d8bf94857c40f
Symforian <32961798+Symforian@users.noreply.github.com>
2018-04-23 16:42:21 +0200

    Temp anims for Noise

--- TeacherVR Prototype/Assets/Events/Scripts/Noise.cs
+++ TeacherVR Prototype/Assets/Events/Scripts/Noise.cs
@@ -6,6 +6,7 @@ public class Noise : Events
 {
     public static bool shoutedLoudEnough = false;
     public static bool doneSomethingLoudEnough = false;
+    private int[] loudStudents;
     //Funkcja po której wywołaniu startuje event
     //Powinna zapamiętać na starcie parametry zmienianych obiektów
     public override void StartEvent()
@@ -17,6 +18,12 @@ public class Noise : Events
         Debug.Log("It's loud now!");
         MicInput.typeOfInput = MicInput.MicInputType.peakDetection;
         //Dodać jakiś dźwięk / hałas, animacja zamieszania wśród studentów?
+        loudStudents = new int[Random.Range(1, GameController.Instance.Students.Students.Length - 1)];
+        for (int i = 0; i < loudStudents.Length; i++)
+        {
+            loudStudents[i] = Random.Range(0, GameController.Instance.Students.Students.Length - 1);
+            GameController.Instance.Students.Students[loudStudents[i]].GetComponentInChildren<AnimationControll>().Talk(true);
+        }
     }
 
     public override void CompleteEvent()
@@ -25,12 +32,20 @@ public class Noise : Events
         Debug.Log("It's quiet now.");
         AddPoints(10);
         Debug.Log("End of noise.");
+        for (int i = 0; i < loudStudents.Length; i++)
+        {
+            GameController.Instance.Students.Students[loudStudents[i]].GetComponentInChildren<AnimationControll>().Talk(false);
+        }
         MicInput.typeOfInput = MicInput.MicInputType.noone;
     }
     public override void AbortEvent()
     {
         base.AbortEvent();
         Debug.Log("End of noise.");
+        for (int i = 0; i < loudStudents.Length; i++)
+        {
+            GameController.Instance.Students.Students[loudStudents[i]].GetComponentInChildren<AnimationControll>().Talk(false);
+        }
         MicInput.typeOfInput = MicInput.MicInputType.noone;
         //Wycofać dodany dźwięk / hałas, animacje zamieszania wśród studentów
     }
@@ -45,7 +60,11 @@ public class Noise : Events
         //GameController.Instance.SoundManager.Play3DAt();
         if (shoutedLoudEnough || doneSomethingLoudEnough)
         {
-           // Debug.Log("Endasd");
+            for (int i = 0; i < loudStudents.Length; i++)
+            {
+                GameController.Instance.Students.Students[loudStudents[i]].GetComponentInChildren<AnimationControll>().Clap();
+            }
+
             CompleteEvent();
         }
 
--- TeacherVR Prototype/Assets/Scripts/Noise/HitVolume.cs
+++ TeacherVR Prototype/Assets/Scripts/Noise/HitVolume.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitVolume : MonoBehaviour {
-    static public int minLevelOfLoudness = 6;
+    public static int minLevelOfLoudness = 6;
 
 
     private void OnCollisionEnter(Collision collision)
