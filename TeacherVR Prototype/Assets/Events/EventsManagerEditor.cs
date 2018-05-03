37d6d666da3d950a0f36bd78a78e0c111ff05baf
Senello <matoz1995@gmail.com>
2018-05-03 16:58:40 +0200

    Buttons for GameController and EventsManager and Random speed for Idle Egg animation

---
+++ TeacherVR Prototype/Assets/Events/EventsManagerEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EventsManager))]
+public class EventsManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EventsManager myScript = (EventsManager) target;
+
+        if (GUILayout.Button("Start next event"))
+        {
+            if (Application.isPlaying) myScript.StartNextEvent();
+            else Debug.LogWarning("Application is not in play mode");
+        }
+        
+        if (GUILayout.Button("Restart events"))
+        {
+            if (Application.isPlaying) myScript.Restart();
+            else Debug.LogWarning("Application is not in play mode");
+        }
+    }
+}
--- TeacherVR Prototype/Assets/Events/Scripts/Doughnut.cs
+++ TeacherVR Prototype/Assets/Events/Scripts/Doughnut.cs
@@ -32,6 +32,13 @@ public class Doughnut : Events
             DoughnutInst = Instantiate(DoughnutObj, MidPoints[0].position, DoughnutObj.transform.rotation);
             DoughnutInst.GetComponent<ResetOnRange>().End = MidPoints[0];
 
+            GameController.Instance.Particles.CreateParticle(Particles.NaszeParticle.Poof, MidPoints[0].position);
+            GameController.Instance.Particles.CreateParticle(Particles.NaszeParticle.Poof,
+                MidPoints[MidPoints.Length - 1].position);
+            GameController.Instance.SoundManager.Play3DAt(SamplesList.ShortPoof, MidPoints[0].position, 0.1f);
+            GameController.Instance.SoundManager.Play3DAt(SamplesList.ShortPoof,
+                MidPoints[MidPoints.Length - 1].position, 0.1f);
+
             CubeInstList.Add(Instantiate(ObjForLine, MidPoints[0].position, ObjForLine.transform.rotation));
 
             foreach (Transform point in MidPoints)
