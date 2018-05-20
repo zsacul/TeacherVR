19e0d3fdee1e4b9a4fe7e146fe216b140acea4db
Laskovsky <masterofpudding@hotmail.com>
2018-05-20 19:16:55 +0300

    Claw in progress

---
+++ TeacherVR Prototype/Assets/Scripts/ClawControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawControl : MonoBehaviour {
+
+    public MovementX X;
+    public MovementY Y;
+    public GEDDAN Claw;
+
+    public float moveX;
+    public float moveY;
+    public float moveClaw;
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+        X.posX = moveX;
+        Y.posY = moveY;
+        Claw.liftdrop = moveClaw;
+    }
+}
---
+++ TeacherVR Prototype/Assets/Scripts/MovementX.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementX : MonoBehaviour {
+
+    public float posX;
+    public float highBorder;
+    public float lowBorder;
+    private Transform transform;
+    private Vector3 trans;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (transform.localPosition.x < highBorder && transform.localPosition.x > lowBorder)
+        {
+
+            PosUpdate();
+        }
+        else
+        {
+            if (transform.localPosition.x >= lowBorder)
+            {
+                if (posX < 0)
+                {
+                    PosUpdate();
+                }
+            }
+            else if (transform.localPosition.x <= highBorder)
+            {
+                if (posX > 0)
+                {
+                    PosUpdate();
+                }
+            }
+        }
+
+
+    }
+
+    void PosUpdate()
+    {
+        trans = new Vector3(posX, 0f, 0f);
+        transform.localPosition += trans;
+    }
+}
+
+
